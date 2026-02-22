1dcf17bf6bcbc8f00e4f58f342e59157c528945a
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-02-23 00:03:05 +0530

    Refactored embedding logic

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from langchain_community.document_loaders import PyPDFLoader
+from rag_with_mongo import Embedding
+
+
 def main():
-    print("Hello from ragwithmongodb!")
+    input_file_path = input("Enter File path :")
+    file_path = rf"{input_file_path}"
+    loader = PyPDFLoader(file_path)
+    data = loader.load()
+    embedding = Embedding()
+    embedding.get_embeddings(data)
 
 
 if __name__ == "__main__":
--- rag_with_mongo.py
+++ rag_with_mongo.py
@@ -1,34 +1,39 @@
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
-from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 import os
 
-load_dotenv() #load variables from.env
-api_key = os.getenv('api_key')
-embedding_model  = HuggingFaceEndpointEmbeddings(
-model="sentence-transformers/all-MiniLM-L6-v2",
-huggingfacehub_api_token=api_key,
-)
+ #load variables from.env
 
-file_path = r"C:\Users\91801\Downloads\sample-report.pdf"
-loader = PyPDFLoader(file_path)
-data = loader.load()
 
-def get_embeddings(data):
-    docs_to_insert = []
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-    document = text_splitter.split_documents(data)
-    texts = [doc.page_content for doc  in document]
-    print(texts[0])
-    embeddings = embedding_model.embed_documents(texts)
-    for text, emb in zip(texts, embeddings):
-        item = {"text":text,"embeddings":emb}
-        docs_to_insert.append(item)
-    print(docs_to_insert[0])
-    return docs_to_insert
 
-get_embeddings(data)
+class Embedding:
+    def __init__(self):
+        load_dotenv()
+        api_key = os.getenv('api_key')
+        if not api_key:
+            raise ValueError("API key not found")
+        self.embedding_model  = HuggingFaceEndpointEmbeddings(
+        model="sentence-transformers/all-MiniLM-L6-v2",
+        huggingfacehub_api_token=api_key,
+        )
+        
+    def get_embeddings(self,data):
+        docs_to_insert = []
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+        documents = text_splitter.split_documents(data)
+        # print("=================",type(documents),"=============\n",documents[0].metadata)
+        texts = [doc.page_content for doc  in documents]
+        # print(texts[0])
+        embeddings = self.embedding_model.embed_documents(texts)
+        for text, emb in zip(texts, embeddings):
+            item = {"text":text,"embeddings":emb}
+            docs_to_insert.append(item)
+        # print(docs_to_insert[0])
+        # print(len(docs_to_insert[0]['embeddings']))
+        return docs_to_insert
+
+
 
 
 
